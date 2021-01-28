4bfa7cc2539a591498ec04793172884fdf862fa9
maneesh <mmmaneesh@gmail.com>
2021-01-29 01:18:20 +0530

    track order

--- src/jApp/service/onesignal.service.ts
+++ src/jApp/service/onesignal.service.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../server/api.service';
 import { LoadingController, Platform, ModalController } from '@ionic/angular';
 import { AppService } from '../app/app.service';
 import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { UtilityService } from '../../jIonic/utility.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class OneSignalService {
   notificationData: any;
 
   constructor(public oneSignal: OneSignal, public modalctrl: ModalController, public app: AppService,
-    public platform: Platform, public Api: ApiService,
+    public platform: Platform, public Api: ApiService, public utility: UtilityService,
     public loader: LoadingController) { 
   
   }
@@ -21,22 +22,41 @@ export class OneSignalService {
   OneSignalInit() {
     this.platform.ready().then( () => {
       if (this.platform.is('cordova')) {
-        this.oneSignal.startInit('4b8c575f-7307-40ea-b4f0-b9eef52c791e', '409717222620'); // import APP ID and Project Number
+
+        // this.oneSignal.startInit('e3fbb154-a2ee-4e3c-9372-611805246f22', '360978708868');
+
+        // this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+        // this.oneSignal.handleNotificationReceived().subscribe(() => {
+        // // do something when notification is received
+        //   this.utility.toast('handleNotificationReceived');
+        // });
+
+        // this.oneSignal.handleNotificationOpened().subscribe(() => {
+        //   // do something when a notification is opened
+        //   this.utility.toast('handleNotificationOpened');
+        // });
+
+        // this.oneSignal.endInit();
+        this.utility.toast('one signal in ');
+        this.oneSignal.startInit('e3fbb154-a2ee-4e3c-9372-611805246f22', '360978708868'); // import APP ID and Project Number
         this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
         this.oneSignal.setSubscription(true);
         this.oneSignal.getIds().then((deviceid) => {
           this.playerid = deviceid.userId;
           this.app.state.set('playerid', deviceid.userId);
           this.app.updateOneSignalId(deviceid.userId);
+          this.utility.toast('one signal dev ' + deviceid.userId);
         });
   
         this.oneSignal.handleNotificationReceived().subscribe((res: any) => {
           console.log('handleNotificationReceived',res);
-  
+          this.utility.toast('handleNotificationReceived ' + res);
         });
   
         this.oneSignal.handleNotificationOpened().subscribe((data: any) => {
           console.log('handleNotificationOpened',data);
+          this.utility.toast('handleNotificationOpened ' + data);
         });
   
         this.oneSignal.endInit();
